a52ef9a553196daf2236710fc0e848ad8a67595d
Pierre Paulo <pierresantista@hotmail.com>
2026-01-08 22:56:23 -0300

    feat(common): add container and section header components

---
+++ src/components/common/SectionHeader.tsx
@@ -0,0 +1,19 @@
+import { cn } from "@/lib/utils";
+
+type SectionHeaderProps = {
+  title: string;
+  subtitle: string;
+};
+
+export function SectionHeader({ title, subtitle }: SectionHeaderProps) {
+  return (
+    <div className={cn("mb-10 max-w-2xl mx-auto text-center")}>
+      <h2 className="text-2xl font-semibold sm:text-3xl lg:text-4xl">
+        {title}
+      </h2>
+      <p className="mt-3 text-base text-muted-foreground sm:text-lg">
+        {subtitle}
+      </p>
+    </div>
+  );
+}
